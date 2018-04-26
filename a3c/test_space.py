a6a689decbdf8b3c375f4117d20e57c751fd1005
Samuel Lavoie <samuel.lavoie.m@gmail.com>
2018-04-26 12:10:47 -0700

    Launch test space on a cluster that actually work

--- a3c/test_space.py
+++ a3c/test_space.py
@@ -3,5 +3,5 @@ import time
 for i in range(1, 101, 2):
     lr = i / 1e4
     print(lr)
-    subprocess.Popen(['sbatch', '-c', '4', '--mem=1000', '--time=719', '--account=def-bengioy', 'start.sh', '--exp-name', 'a3c_space_sn_actor_depth_3_lr_%s' % lr, '--use-sn-actor', 'True', '--num-processes', '16' '--depth', '${d}', '--num-episodes', '400', '--lr', str(lr), '--root-path', '/home/lavoiems/scratch/a3c'])
+    subprocess.Popen(['sbatch', '-c', '4', '--mem=4000', '--time=719', '--account=def-bengioy', 'start.sh', '--exp-name', 'a3c_space_sn_actor_depth_3_lr_%s' % lr, '--use-sn-actor', 'True', '--num-processes', '16', '--depth', '3', '--num-episodes', '400', '--lr', str(lr), '--root-path', '/home/lavoiems/scratch/a3c', '--env-name', 'SpaceInvaders-v4'])
     time.sleep(5)
