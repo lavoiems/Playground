7a29adebc7c7b988b35aa026f4aea7b185e58333
Samuel Lavoie <samuel.lavoie.m@gmail.com>
2018-04-25 17:18:27 -0700

    Fix main visdom

--- a3c/start.sh
+++ a3c/start.sh
@@ -3,6 +3,8 @@ export HOME=`getent passwd $USER | cut -d':' -f6`
 source ~/.bashrc
 export PYTHONUNBUFFERED=1
 echo Running on $HOSTNAME
-echo Running script $1
+
+module load miniconda3
+source activate py36
 
 python main.py --server http://samuel.zapto.org --port 14445 "$@"
